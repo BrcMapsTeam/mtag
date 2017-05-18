ff08354e197ac84f8ed88bdee322ed9136c0994c
Zibethin <nguyenththuong@gmail.com>
2017-05-18 18:35:57 +0100

    ajax call when page loads, displays all data on page

--- main-acronym.js
+++ main-acronym.js
@@ -1,31 +1,23 @@
 var acronymContainer = document.getElementById("acronym-info");
 var btn = document.getElementById("btn");
-
-btn.addEventListener("click", function() {
-
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://proxy.hxlstandard.org/data.json?force=on&url=https%3A//docs.google.com/spreadsheets/d/1LBQDNAi4EaD72FM5e4esmU30OxZbBcUgJPAgxNaM7G0/edit%23gid%3D1730333599&strip-headers=on'); /* Link JSON File */
-
-    ourRequest.onload = function() {
-        var ourData = JSON.parse(ourRequest.responseText);
-        renderHTML(ourData);
-    };
-    ourRequest.send();
-});
-
 var x;
 
-function myFunction() {
+var call = $.ajax({ 
+    type: 'GET', 
+    url: 'https://proxy.hxlstandard.org/data.json?force=on&url=https%3A//docs.google.com/spreadsheets/d/1LBQDNAi4EaD72FM5e4esmU30OxZbBcUgJPAgxNaM7G0/edit%23gid%3D1730333599&strip-headers=on',
+    dataType: 'json',
+});
+
+
+$.when(call).then(function(data){
+    var ourData = JSON.parse(call.responseText);
+    renderHTML(ourData);
+});
+
+
+function filtering() {
     x = document.getElementById("mtagInput").value;
 
-}
-
-function renderHTML(data) {
-    var htmlString = "";
-    var didyoufindit; /* Create a new one */
-    var numberofdefinition = 0; /* Amount of same definition */
-    var definition = [] ; /* Result array */
-    
     for (i = 1; i < data.length; i++) {
         htmlString = data[i][1];
 
@@ -34,18 +26,34 @@ function renderHTML(data) {
             numberofdefinition += 1; /* counts up if a another is found */
             didyoufindit = "yes";
         }
-  }
-        var text = ""; 
-        for (i = 0; i < definition.length; i++) {
-            text += (i+1) + "." + "Definition" + definition[i];
-        } /* Search for equal Definition" */
+    }
+
+    var text = "";
+    for (i = 0; i < definition.length; i++) {
+        text += (i + 1) + "." + "Definition" + definition[i];
+    } /* Search for equal Definition" */
 
     document.getElementById("showdata").innerHTML = text; /* Print the different definition */
-    
+
     if (didyoufindit != "yes") {
         document.getElementById("showdata").innerHTML = ("Create a new one"); /* Create a new one as a text */
     }
+}
+
+function renderHTML(data) {
+    var htmlString = "<ul>";
+    var didyoufindit; /* Create a new one */
+    var numberofdefinition = 0; /* Amount of same definition */
+    var definition = []; /* Result array */
+    console.log(data);
+    data.forEach(function(c, i){
+        htmlString += "<li>" + c[2] + "<p>" + c[3] + "<a href='" + c[4] + "'>" + c[4] + "</a></li>";
+    })
+    htmlString += "</ul>";
+    document.getElementById("showdata").innerHTML = htmlString;
 } /* If there is no Acronym, the text "Create a new one" will displayed therefore didyoufindit != "yes"  */
+
+
 
 
 /*Generate acronym*/
